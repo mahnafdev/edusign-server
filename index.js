b44c697ded601a11933732c727b5fff9f09a2a58
mahnaf <mahnaf125@gmail.com>
2025-06-11 01:24:53 +0600

    chore: connect mongodb database

--- index.js
+++ index.js
@@ -1,11 +1,45 @@
 const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const dotenv = require("dotenv").config;
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5100;
+dotenv();
 
 // Middlewares
 app.use(express.json());
 app.use(cors());
+
+// MongoDB URI
+const db_uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@professorcluster.rlegbqz.mongodb.net/?retryWrites=true&w=majority&appName=ProfessorCluster`;
+
+// MongoClient
+const db_client = new MongoClient(db_uri, {
+	serverApi: {
+		version: ServerApiVersion.v1,
+		strict: true,
+		deprecationErrors: true,
+	},
+});
+
+// MongoDB run function
+async function run_db() {
+	try {
+		// Client connection with server (turn off in deployment)
+		await db_client.connect();
+		// Define Database
+		const database = db_client.db("edusign");
+		// Define Collections
+		const assignmentsCollection = database.collection("assignments");
+		// Ping for successful connection confirmation
+		await db_client.db("admin").command({ ping: 1 });
+		console.log("Pinged. Successfully connected to MongoDB!");
+	} finally {
+		// Don't close client connection with server
+		// await client.close();
+	}
+}
+run_db().catch(console.dir);
 
 // Home route response
 app.get("/", (req, res) => {
